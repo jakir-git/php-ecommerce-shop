23b23642f7545dde0cdd156a0d878d595f877a0a
jakir-git <carrier.jakir@gmail.com>
2022-12-11 17:01:20 +0600

    Porject admin dashboard setup

---
+++ admin/includes/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer pt-5">
+      <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-12 mb-lg-0 mb-4">
+            <div class="copyright text-center text-sm text-muted">
+              © <script>
+                document.write(new Date().getFullYear())
+              </script>,
+              made with <i class="fa fa-heart"></i> by
+              <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Jakir</a>
+              for a better web.
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </main>
+
+  <script src="../assets/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/js/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/smooth-scrollbar.min.js"></script>
+
+  </body>
+</html>
